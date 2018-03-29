592186c660e522691933818ebf059f00df397a00
teppei <>
2018-03-29 17:15:38 +0900

    ポプテピピック ランダム表示 stop

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-//
+// 配列宣言
+var array = ["ポ", "プ", "テ", "ピピック"];
+
+var pipimi = document.getElementById("pipimi");
+var popuko = document.getElementById("popuko");
+
+var add = function(){
+
+    var random = Math.floor(Math.random() * array.length);  // スコープ外だとNG
+    pipimi.innerHTML += array[random];
+    var longWord = pipimi.innerHTML;
+
+    if(longWord.endsWith("ポプテピピック")){
+        popuko.innerHTML = "end";
+        clearInterval(start);
+    }
+};
+
+var start = setInterval(add, 10);
