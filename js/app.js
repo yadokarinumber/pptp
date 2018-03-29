143c605615d76d8059ed7f15d9b9570c9490b67c
teppei <>
2018-03-29 17:47:27 +0900

    push で起動

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 // 配列宣言
 var array = ["ポ", "プ", "テ", "ピピック"];
-
+var btn = document.getElementById("btn");
 var pipimi = document.getElementById("pipimi");
 var popuko = document.getElementById("popuko");
 
-var add = function(){
 
-    var random = Math.floor(Math.random() * array.length);  // スコープ外だとNG
-    pipimi.innerHTML += array[random];
-    var longWord = pipimi.innerHTML; // 変数に入れたほうが良いの？
+function push(){
 
-    if(longWord.endsWith("ポプテピピック")){
-        popuko.innerHTML = "end";
-        clearInterval(start);
-    }
-};
+    function add(){
 
-var start = setInterval(add, 10);
+        var random = Math.floor(Math.random() * array.length);  // スコープ外だとNG
+        pipimi.innerHTML += array[random];
+        var longWord = pipimi.innerHTML; // 変数に入れたほうが良いの？
+
+        if(longWord.endsWith("ポプテピピック")){
+            popuko.innerHTML = "end";
+            clearInterval(start);
+        }
+    };
+
+    var start = setInterval(add, 10);
+}
+
+var a = btn.addEventListener("click", push)
